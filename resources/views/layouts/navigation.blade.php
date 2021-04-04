bf9dfb591acbb2f6cfe9fd4998aea3c7a6a7d429
marianorapa <marianorapaport@gmail.com>
2021-04-03 21:44:38 -0300

    change products link in navbar

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -17,7 +17,7 @@
                     </x-nav-link>
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('products.create')" :active="request()->routeIs('products.create')">
+                    <x-nav-link :href="route('products.index')" :active="request()->routeIs('products.index')">
                         {{ __('Productos') }}
                     </x-nav-link>
                 </div>
